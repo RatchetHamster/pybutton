54248157878367a4dcbe8790995f0e95bdfde81a
RatchetHamster <94993719+RatchetHamster@users.noreply.github.com>
2026-01-08 09:26:31 +0000

    Update main.py

--- main.py
+++ main.py
@@ -6,14 +6,18 @@ import os
 
 button = Button(23, pull_up=True, hold_time=2, hold_repeat=False)
 
+def immediate_press():
+    pass
+
 def short_press():
     os.system("sudo reboot -h now")
 
 def long_press():
     os.system("sudo shutdown -h now")
 
-button.when_pressed = None          # not used
+button.when_pressed = immediate_press
 button.when_released = short_press  # fires only if not held
 button.when_held = long_press
 pause()
+
 
